05130527f1c1b3a958203cf6ddb6fb0062a60016
srinivasch2202 <srinivasch2202@gmail.com>
2023-11-13 21:42:34 +0530

    methods

--- 2.array_atd/z2_array_adt.c
+++ 2.array_atd/z2_array_adt.c
@@ -3,11 +3,18 @@
 
 struct array
 {
-
+    int *p;
+    int size;
+    int length;
 };
 
 void main()
 {
-   
+    struct array arr={{1,2,3,4},10,4};
+    printf("%d\n",arr.p);
+    for(int i=0;i<4;i++)
+    {
+        printf("%d\t",&arr.p[i]);
+    }
 
 }
