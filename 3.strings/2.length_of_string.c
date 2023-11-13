47aca9a17725dc8095146ddf494c87e907a97c67
srinivasch2202 <srinivasch2202@gmail.com>
2023-11-14 00:01:47 +0530

    finding the length

---
+++ 3.strings/2.length_of_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char *name="sri";
+    // printf("enter the name");
+    // scanf("%s",&*name); i am missing something here
+    int i;
+    for(i=0;name[i]!='\0';i++)
+    {
+
+    }
+    printf("%d\n",i);
+
+}
